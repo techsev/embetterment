27e833d0d557a31651c6fbe8bbd935c587a1c77f
techsev <techsev@gmail.com>
2013-10-28 14:18:01 -0400

    Many node & grunt changes + testing command line git commit

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Date: 2013-10-22
 
 Created the first section of the site, my personal and carreer progress tracker. I used SASS and Compass to create a static page that using a few mixins and math variables was able to have easy to customize progress bars. I also created a pen on www.codepen.io and embed it on the site show how I created it. I used a bunch of different carreer and life goals that, if you were to look in the `_progress.scss` file here on github, you might find entertaining.  
 
+
+To do next: Node.js + Grunt.js to updated certain progress based on outside variables and api calls
 -Seve
---
+++ gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    getWeight: {},
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'sass',
+          cssDir: 'stylesheets',
+          environment: 'production'
+        }
+      },
+      dev: {                    // Another target
+        options: {
+          sassDir: 'sass',
+          cssDir: 'stylesheets'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+
+
+
+  // grunt.registerTask('default', ['compass:dev']);
+grunt.registerTask('default', ['getWeight','compass:dev', 'gitcheckout']);
+ grunt.registerTask('getWeight', 'User twitter API call to get weight', function() {
+  
+  var done = this.async();
+
+  var twitter = require('mtwitter');
+  var fs = require('fs');
+
+
+var twit = new twitter({
+  consumer_key: 'XClhiEf72XxQwMKPA2pKXA',
+  consumer_secret: '5U1aPB6w77CnFHc1N8RjxKqB3YDCaiwIdQnyDs9OHM',
+  access_token_key: '2153919734-rPfaCwVTamO9SlZMC4txcl5LexpNTg2uKca1lsY',
+  access_token_secret: 'veosn7CB4t2dJc56k8kVoGWtOtULnYdkqmySAXqqPlw00'
+});
+
+ twit.get('/statuses/user_timeline', { include_entities: true },
+                function (err, data) {
+                    if (err) {
+                        console.log(err.toString());
+                    }
+                    else {
+                      console.log(data[0].text);
+                      var tempString = data[0].text.split('s lost ');
+                      tempString = tempString[1].split(' lbs');
+                      weightLost = tempString[0];
+                      console.log(weightLost);
+                      fs.unlinkSync('sass/_mfpWeight.scss');
+                      fs.writeFile('sass/_mfpWeight.scss', "$weight-lost: " + weightLost + ";", function(err) {
+              if(err) {
+                  console.log(err);
+              } else {
+                  console.log("The file was saved!");
+                  done();
+              }
+          }); 
+        }
+      });
+  });
+  
+
+
+};
