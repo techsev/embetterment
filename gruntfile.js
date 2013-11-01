d2887bf3b8add7651d2e768ee7b73320ef76e050
techsev <techsev@gmail.com>
2013-11-01 13:39:30 -0400

    Lots of design changes, added content to page, also got auto progress post to twitter working with grunt.

--- gruntfile.js
+++ gruntfile.js
@@ -20,13 +20,14 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-compass');
+  
 
 
 
 
   // grunt.registerTask('default', ['compass:dev']);
-grunt.registerTask('default', ['getWeight','compass:dev']);
- grunt.registerTask('getWeight', 'User twitter API call to get weight', function() {
+grunt.registerTask('default', ['getWeight','compass:dev','postProgress']);
+grunt.registerTask('getWeight', 'Use twitter API call to get weight', function() {
   
   var done = this.async();
 
@@ -64,6 +65,54 @@ var twit = new twitter({
         }
       });
   });
+
+grunt.registerTask('postProgress','Use Twitter API to post overall progress percentage',function(){
+var done = this.async();
+  var twitter = require('mtwitter');
+
+  var twit = new twitter({
+  consumer_key: 'l3wVICTGFLK8lwoZdWlApA',
+  consumer_secret: 'N1G0LEjvmn6P2458F4aMb8OSSu0Qg590ywQWAI',
+  access_token_key: '18638308-id1rSKnWCBIZVwHKDbZ37AsXu9GC8sSmVDkybX1H6',
+  access_token_secret: 'hv5O1J18oN1SvP6G1G79uP2Agp9zG5RYll9I87WZwLuJe'
+});
+
+  var parse = require('css-parse')
+  , fs = require('fs')
+  , read = fs.readFileSync
+  , css = read('stylesheets/screen.css', 'utf8');
+  var css_json = parse(css); 
+  var overall_progress;
+  for (var i = css_json.stylesheet.rules.length - 1; i >= 0; i--) {
+    if (css_json.stylesheet.rules[i].type == 'rule' && css_json.stylesheet.rules[i].selectors[0] == ".OVERALL-PROGRESS div.progressBar div" )
+    {
+      overall_progress = css_json.stylesheet.rules[i].declarations[0].value;
+      break;
+    }
+  };
+
+  if (overall_progress != null)
+  {
+    console.log(overall_progress)
+
+
+    var content = {status: 'My Current Life Goal Progress: ' + overall_progress + ' #TheBettermentOfSeve techsev.github.com/embetterment'};
+    twit.post('statuses/update', content, function(err, item) {
+      console.log(err, item);
+      done();
+    });
+
+  } else {
+    console.log('error');
+    done();
+  }
+
+
+
+
+
+
+});
   
 
 
